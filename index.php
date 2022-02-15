69c4998539bdaabd37bbe468b5b50ba9428c8c46
Peter <006815296@coyote.csusb.edu>
2022-02-14 18:35:38 -0800

    populating content

--- index.php
+++ index.php
@@ -64,3 +64,32 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
+
+<?php
+
+// Read the JSON file
+$json_file = file_get_contents('my_data.json');
+
+// Decode the JSON file
+$json_data = json_decode($json_file,true);
+
+?>
+
+<!-- Peter Romera-Messina --!>
+<h1>
+<?php echo $json_data['Peter']; ?>
+    <span class="text-primary"><?php echo $json_data['Romera-Messina']; ?></span>
+</h1>
+
+<?php foreach($json_data['experience'] AS $experience) { ?>
+  <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+    <div class="flex-grow-1">
+      <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
+      <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+      <p><?php echo $experience['description']; ?></p>
+    </div>
+    <div class="flex-shrink-0">
+      <span class="text-primary"><?php echo $experience['period']; ?></span>
+    </div>
+  </div>
+<?php  } ?>
